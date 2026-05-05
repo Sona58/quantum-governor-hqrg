032edd736a33db995d5b72c6848e4ed212245900
Sonali Chawla <s2sonali@gmail.com>
2026-05-05 08:07:48 +0000

    Fixing CI-CD

--- tests/unit/test_accounting.py
+++ tests/unit/test_accounting.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 
-from services.cost_analyzer.src.accounting import calculate_credits
+from services.cost_analyzer.src.accounting import calculate_cost
 
 def test_cost_multiplier_logic():
     # Simulators should be cheap (e.g., 1.0 multiplier)
-    sim_cost = calculate_credits(runtime_sec=10, engine="qiskit-aer-sim")
+    sim_cost = calculate_cost(duration=10, engine="qiskit-aer-sim")
     # QPU should be expensive (e.g., 5.0 multiplier)
-    qpu_cost = calculate_credits(runtime_sec=10, engine="ibm-qpu")
+    qpu_cost = calculate_cost(duration=10, engine="ibm-qpu")
     
     assert qpu_cost > sim_cost
